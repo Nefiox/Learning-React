2c1856fe1b1afdb8dd599e51b64778703923f552
Juan Jiménez <nefioxdev@gmail.com>
2021-09-16 23:01:53 -0500

    PropTypes & DefaultProps

--- src/components/Pelicula.js
+++ src/components/Pelicula.js
@@ -1,21 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Pelicula(props) {
-
-
-
+    let listadoDeGeneros; 
+    if(props.generos != null) {
+        listadoDeGeneros = 
+        <ul>
+        { props.generos.map((genre, i) => {
+            return <li key = {genre + i}> {genre} </li>
+        })}
+    </ul>
+    } else {
+        listadoDeGeneros = ""
+    }
     return (
         <div>
             <h2>{props.titulo}</h2>
             <p>Rating: {props.rating}</p>
             <p>Awards: {props.awards}</p>
-            <ul>
-                { props.genres.map((genre, i) => {
-                    return <li key = {genre + i}> {genre} </li>
-                })}
-            </ul>
+            { listadoDeGeneros }
         </div>
     );
   }
+
+Pelicula.defaultProps = {
+    rating: 'Esta película aún no fue puntuada',
+    generos: null
+}
+
+Pelicula.propTypes = {
+    rating: PropTypes.string,
+    titulo: PropTypes.string,
+    awards: PropTypes.string,
+    genre: PropTypes.string
+}
 
 export default Pelicula;
--- src/components/TiraPeliculas.js
+++ src/components/TiraPeliculas.js
@@ -5,10 +5,10 @@ import Pelicula from './Pelicula';
 function TiraPeliculas() {
     return (
         <div>
-            <Pelicula titulo = "Avengers" rating="9.2" awards="3" genres={["Ciencia ficción", "Misterio"]} />
-            <Pelicula titulo = "Parasite" rating="10" awards="10" genres={["Acción", "Drama"]} />
-            <Pelicula titulo = "Toy Story" rating="8" awards="4" genres={["Drama", "Ciencia ficción"]} />
-            <Pelicula titulo = "Her" rating="6" awards="1" genres={["Acción", "Misterio"]} />
+            <Pelicula titulo = "Avengers" rating="9.2" awards="3" generos={["Ciencia ficción", "Misterio"]} />
+            <Pelicula titulo = "Parasite" rating="10" awards="10" generos={["Acción", "Drama"]} />
+            <Pelicula titulo = "Toy Story" rating="8" awards="4" generos={["Drama", "Ciencia ficción"]} />
+            <Pelicula titulo = "Her" rating="6" awards="1" generos={["Acción", "Misterio"]} />
         </div>
     );
   }
