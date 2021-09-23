3a6bc72fce92c131e32046410b2ab04daa5d121c
Juan Jiménez <nefioxdev@gmail.com>
2021-09-22 21:22:55 -0500

    Events

--- src/components/TiraPeliculas.js
+++ src/components/TiraPeliculas.js
@@ -3,9 +3,23 @@ import Pelicula from './Pelicula';
 
 
 class TiraPeliculas extends Component {
+    lanzarAlerta() {
+        alert('Click, click!')
+    }
+
+    cambiarAmarillo() {
+        document.querySelector("body").style.backgroundColor = "yellow";
+    }
+    
+    cambiarARosa() {
+        document.querySelector("body").style.backgroundColor = "pink";
+
+    }
+
+
     render() {
         return (
-        <div>
+        <div onClick={ this.lanzarAlerta } onMouseOver={ this.cambiarAmarillo } onMouseOut={ this.cambiarARosa }>
             <Pelicula titulo = "Avengers" rating="9.2" awards="3" generos={["Ciencia ficción", "Misterio"]} />
             <Pelicula titulo = "Parasite" rating="10" awards="10" generos={["Acción", "Drama"]} />
             <Pelicula titulo = "Toy Story" rating="8" awards="4" generos={["Drama", "Ciencia ficción"]} />
